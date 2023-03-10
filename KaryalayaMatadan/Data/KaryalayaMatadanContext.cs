73bc109cfc7bf95f42e809ecf8e1a07e4c25043b
Prajwol Chhetri <prajwolc391@gmail.com>
2023-03-10 09:07:19 +0545

    Setup DB Context

---
+++ KaryalayaMatadan/Data/KaryalayaMatadanContext.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace KaryalayaMatadan.Data
+{
+    public class KaryalayaMatadanContext : DbContext
+    {
+        public KaryalayaMatadanContext() : base("name=KaryalayaMatadanContext")
+        {
+        }
+
+    }
+}
